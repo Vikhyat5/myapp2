d9110aff0ff55850f882ac6f1cec4ba21f50b7b6
vikhyat5 <vikhyatmidha5@gmail.com>
2019-05-11 12:29:51 +0530

    Updates

---
+++ precache-manifest.d5bee2aa2238f2e4bc817eed00b252e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb4f389631f8d77c819a5c6a3a962fa7",
+    "url": "/myapp2/index.html"
+  },
+  {
+    "revision": "7a1eaa42c6d4a639baff",
+    "url": "/myapp2/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/myapp2/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "7a1eaa42c6d4a639baff",
+    "url": "/myapp2/static/js/main.f1f49218.chunk.js"
+  },
+  {
+    "revision": "a3c1f14dc44347eac98d",
+    "url": "/myapp2/static/js/runtime~main.0215af8a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp2/static/media/logo.5d5d9eef.svg"
+  }
+]);
